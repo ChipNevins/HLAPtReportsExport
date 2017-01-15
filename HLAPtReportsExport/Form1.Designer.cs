57654339454bddc674abb66079046fb729c63cc6
Chip Nevins <chip.nevins@gmail.com>
2017-01-15 13:03:38 -0500

    Build reports only when Rnum found, report name is R-number. Use HLA_Reports db with current data

--- HLAPtReportsExport/Form1.Designer.cs
+++ HLAPtReportsExport/Form1.Designer.cs
@@ -31,8 +31,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.cbTop20 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -61,14 +64,6 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Enter Report Year";
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(115, 62);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(496, 212);
-            this.listBox1.TabIndex = 10;
-            // 
             // cbTop20
             // 
             this.cbTop20.AutoSize = true;
@@ -79,13 +74,48 @@
             this.cbTop20.Text = "Top 20";
             this.cbTop20.UseVisualStyleBackColor = true;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(327, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Row: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Current report";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 584);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.cbTop20);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -100,8 +130,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.CheckBox cbTop20;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- HLAPtReportsExport/Form1.cs
+++ HLAPtReportsExport/Form1.cs
@@ -30,10 +30,10 @@ namespace HLAPtReportsExport
             // call builder with one pk
             int reportYear, reportPk;
             int nbrReportsWritten = 0;
-            string reportName;
+            string reportName = "";
             if (Int32.TryParse(textBox1.Text, out reportYear))
             {
-                string CS = ConfigurationManager.ConnectionStrings["HLA_DBCS"].ConnectionString;
+                string CS = ConfigurationManager.ConnectionStrings["HLA_Reports"].ConnectionString;
                 DataSet dsReportPks = new DataSet();
                 using (SqlConnection conPk = new SqlConnection(CS))
                 {
@@ -45,8 +45,10 @@ namespace HLAPtReportsExport
                     {
                         if (cbTop20.Checked & (++nbrReportsWritten > 20)) break;
                         Int32.TryParse(dr.ItemArray[0].ToString(), out reportPk);
-                        reportName = dr["strreportname"].ToString();
-                        BuildReport(reportName, reportPk);
+                        reportName = BuildReport(reportPk);
+                        textBox3.Text = reportName;
+                        textBox2.Text = "Row " + nbrReportsWritten.ToString();
+                        Application.DoEvents();
                     }
                 }
             }
@@ -60,11 +62,12 @@ namespace HLAPtReportsExport
 
         }
 
-        private void BuildReport(string reportName, int aReportPk)
+        private string BuildReport(int aReportPk)
         {
-            string CS = ConfigurationManager.ConnectionStrings["HLA_DBCS"].ConnectionString;
+            string CS = ConfigurationManager.ConnectionStrings["HLA_Reports"].ConnectionString;
             DateTime dt;
             string fileName;
+            bool isRnumFound = false;
             DataSet ds = new DataSet();
 
             using (SqlConnection con = new SqlConnection(CS))
@@ -87,122 +90,130 @@ namespace HLAPtReportsExport
                     if (dr["strrefnum"].ToString() !="")
                     {
                         fileName = "R" + dr["strrefnum"].ToString();
+                        isRnumFound = true;
                     }
 
                 }
 
             }
-            using (Document document = new Document())
+            if (isRnumFound)
             {
-                string myPath = ConfigurationManager.AppSettings["Path"];
-                PdfWriter.GetInstance(document, new FileStream(myPath + fileName + ".pdf", FileMode.OpenOrCreate));
-                document.Open();
-                iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
-                iTextSharp.text.Font font6 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 6);
-                iTextSharp.text.Font font14 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14);
-                document.Add(new Paragraph(fileName, font14));
-
-                foreach (DataRow drRep in ds.Tables["ReportHdrs"].Rows)
+                using (Document document = new Document())
                 {
-                    // Header table
-                    PdfPTable tableRep = new PdfPTable(7);
-                    tableRep.SpacingBefore = 14;
-                    tableRep.SetWidths(new int[] { 12, 70, 30, 40, 12, 12, 8 });
-                    tableRep.AddCell(new Phrase("Version", font6));
-                    tableRep.AddCell(new Phrase("Report Name", font6));
-                    tableRep.AddCell(new Phrase("Provider Name", font6));
-                    tableRep.AddCell(new Phrase("Hospital Name", font6));
-                    tableRep.AddCell(new Phrase("Original Date", font6));
-                    tableRep.AddCell(new Phrase("Updated Date", font6));
-                    tableRep.AddCell(new Phrase("PK", font6));
-                    tableRep.AddCell(new Phrase(drRep["pkReportSeq"].ToString(), font5));
-                    tableRep.AddCell(new Phrase(drRep["strreportname"].ToString(), font5));
-                    tableRep.AddCell(new Phrase(drRep["strProviderName"].ToString(), font5));
-                    tableRep.AddCell(new Phrase(drRep["strHospitalName"].ToString(), font5));
-                    tableRep.AddCell(new Phrase(DateString(drRep["dtoriginaldate"].ToString()), font5));
-                    tableRep.AddCell(new Phrase(DateString(drRep["dtupdateddate"].ToString()), font5));
-                    tableRep.AddCell(new Phrase(drRep["pkReportId"].ToString(), font5));
-                    document.Add(tableRep);
+                    string myPath = ConfigurationManager.AppSettings["Path"];
+                    PdfWriter.GetInstance(document, new FileStream(myPath + fileName + ".pdf", FileMode.OpenOrCreate));
+                    document.Open();
+                    iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
+                    iTextSharp.text.Font font6 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 6);
+                    iTextSharp.text.Font font14 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 14);
+                    document.Add(new Paragraph(fileName, font14));
 
-                    // Items table
-                    PdfPTable tableItem = new PdfPTable(11);
-                    tableItem.SetWidths(new int[] { 30, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
-                    tableItem.SpacingAfter = 5;
-                    tableItem.AddCell(new Phrase("Name", font6));
-                    tableItem.AddCell(new Phrase("Relation", font6));
-                    tableItem.AddCell(new Phrase("R#", font6));
-                    tableItem.AddCell(new Phrase("Coll. Date", font6));
-                    tableItem.AddCell(new Phrase("Recv'd Date", font6));
-                    tableItem.AddCell(new Phrase("A", font6));
-                    tableItem.AddCell(new Phrase("B", font6));
-                    tableItem.AddCell(new Phrase("C", font6));
-                    tableItem.AddCell(new Phrase("DRB1", font6));
-                    tableItem.AddCell(new Phrase("DRB345", font6));
-                    tableItem.AddCell(new Phrase("DQB1", font6));
-                    foreach (DataRow drItem in ds.Tables["ReportItems"].Rows)
+                    foreach (DataRow drRep in ds.Tables["ReportHdrs"].Rows)
                     {
-                        if (drRep.ItemArray[1].ToString() == drItem.ItemArray[1].ToString())
-                        {
-                            tableItem.AddCell(new Phrase(drItem["strPersonname"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strRelationToPt"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strRefNum"].ToString(), font5));
-                            if (drItem["sp_bleed"].ToString() == "")
-                            {
-                                tableItem.AddCell(new Phrase(" ", font5));
-                            }
-                            else
-                            {
-                                dt = DateTime.Parse(drItem["sp_bleed"].ToString());
-                                tableItem.AddCell(new Phrase(dt.ToShortDateString(), font5));
-                            }
-                            if (drItem["sp_accession"].ToString() == "")
-                            {
-                                tableItem.AddCell(new Phrase(" ", font5));
-                            }
-                            else
-                            {
-                                dt = DateTime.Parse(drItem["sp_accession"].ToString());
-                                tableItem.AddCell(new Phrase(dt.ToShortDateString(), font5));
-                            }
-                            tableItem.AddCell(new Phrase(drItem["strA1"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strB1"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strCw1"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDrb11"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDRB3451"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDqb11"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(" ", font5));
-                            tableItem.AddCell(new Phrase(" ", font5));
-                            tableItem.AddCell(new Phrase(" ", font5));
-                            tableItem.AddCell(new Phrase(" ", font5));
-                            tableItem.AddCell(new Phrase(" ", font5));
-                            tableItem.AddCell(new Phrase(drItem["strA2"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strB2"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strCw2"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDrb12"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDRB3452"].ToString(), font5));
-                            tableItem.AddCell(new Phrase(drItem["strDqb12"].ToString(), font5));
-                        }
-                    }
-                    document.Add(tableItem);
+                        // Header table
+                        PdfPTable tableRep = new PdfPTable(7);
+                        tableRep.SpacingBefore = 14;
+                        tableRep.SetWidths(new int[] { 12, 65, 30, 40, 15, 15, 8 });
+                        tableRep.AddCell(new Phrase("Version", font6));
+                        tableRep.AddCell(new Phrase("Report Name", font6));
+                        tableRep.AddCell(new Phrase("Provider Name", font6));
+                        tableRep.AddCell(new Phrase("Hospital Name", font6));
+                        tableRep.AddCell(new Phrase("Original Date", font6));
+                        tableRep.AddCell(new Phrase("Updated Date", font6));
+                        tableRep.AddCell(new Phrase("PK", font6));
+                        tableRep.AddCell(new Phrase(drRep["pkReportSeq"].ToString(), font5));
+                        tableRep.AddCell(new Phrase(drRep["strreportname"].ToString(), font5));
+                        tableRep.AddCell(new Phrase(drRep["strProviderName"].ToString(), font5));
+                        tableRep.AddCell(new Phrase(drRep["strHospitalName"].ToString(), font5));
+                        tableRep.AddCell(new Phrase(DateString(drRep["dtoriginaldate"].ToString()), font5));
+                        tableRep.AddCell(new Phrase(DateString(drRep["dtupdateddate"].ToString()), font5));
+                        tableRep.AddCell(new Phrase(drRep["pkReportId"].ToString(), font5));
+                        document.Add(tableRep);
 
-                    PdfPTable tableComment = new PdfPTable(2);
-                    tableComment.SetWidths(new int[] { 10, 100 });
-                    tableComment.SpacingAfter = 20;
-                    tableComment.AddCell(new Phrase("Date", font6));
-                    tableComment.AddCell(new Phrase("Comment", font6));
-                    foreach (DataRow drComment in ds.Tables["ReportComments"].Rows)
-                    {
-                        if (drRep.ItemArray[1].ToString() == drComment.ItemArray[1].ToString())
+                        // Items table
+                        PdfPTable tableItem = new PdfPTable(11);
+                        tableItem.SetWidths(new int[] { 30, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 });
+                        tableItem.SpacingAfter = 5;
+                        tableItem.AddCell(new Phrase("Name", font6));
+                        tableItem.AddCell(new Phrase("Relation", font6));
+                        tableItem.AddCell(new Phrase("R#", font6));
+                        tableItem.AddCell(new Phrase("Coll. Date", font6));
+                        tableItem.AddCell(new Phrase("Recv'd Date", font6));
+                        tableItem.AddCell(new Phrase("A", font6));
+                        tableItem.AddCell(new Phrase("B", font6));
+                        tableItem.AddCell(new Phrase("C", font6));
+                        tableItem.AddCell(new Phrase("DRB1", font6));
+                        tableItem.AddCell(new Phrase("DRB345", font6));
+                        tableItem.AddCell(new Phrase("DQB1", font6));
+                        foreach (DataRow drItem in ds.Tables["ReportItems"].Rows)
                         {
-                            tableComment.AddCell(new Phrase(DateString(drComment["dtCommentDate"].ToString()), font5));
-                            tableComment.AddCell(new Phrase(drComment["strCommentText"].ToString(), font5));
+                            if (drRep.ItemArray[1].ToString() == drItem.ItemArray[1].ToString())
+                            {
+                                tableItem.AddCell(new Phrase(drItem["strPersonname"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strRelationToPt"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strRefNum"].ToString(), font5));
+                                if (drItem["dtAccessionDate"].ToString() == "")
+                                {
+                                    tableItem.AddCell(new Phrase(" ", font5));
+                                }
+                                else
+                                {
+                                    dt = DateTime.Parse(drItem["dtAccessionDate"].ToString());
+                                    tableItem.AddCell(new Phrase(dt.ToShortDateString(), font5));
+                                }
+                                if (drItem["dtReceived"].ToString() == "")
+                                {
+                                    tableItem.AddCell(new Phrase(" ", font5));
+                                }
+                                else
+                                {
+                                    dt = DateTime.Parse(drItem["dtReceived"].ToString());
+                                    tableItem.AddCell(new Phrase(dt.ToShortDateString(), font5));
+                                }
+                                tableItem.AddCell(new Phrase(drItem["strA1"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strB1"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strCw1"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDrb11"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDRB3451"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDqb11"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(" ", font5));
+                                tableItem.AddCell(new Phrase(" ", font5));
+                                tableItem.AddCell(new Phrase(" ", font5));
+                                tableItem.AddCell(new Phrase(" ", font5));
+                                tableItem.AddCell(new Phrase(" ", font5));
+                                tableItem.AddCell(new Phrase(drItem["strA2"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strB2"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strCw2"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDrb12"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDRB3452"].ToString(), font5));
+                                tableItem.AddCell(new Phrase(drItem["strDqb12"].ToString(), font5));
+                            }
                         }
+                        document.Add(tableItem);
+
+                        PdfPTable tableComment = new PdfPTable(2);
+                        tableComment.SetWidths(new int[] { 10, 100 });
+                        tableComment.SpacingAfter = 20;
+                        tableComment.AddCell(new Phrase("Date", font6));
+                        tableComment.AddCell(new Phrase("Comment", font6));
+                        foreach (DataRow drComment in ds.Tables["ReportComments"].Rows)
+                        {
+                            if (drRep.ItemArray[1].ToString() == drComment.ItemArray[1].ToString())
+                            {
+                                tableComment.AddCell(new Phrase(DateString(drComment["dtCommentDate"].ToString()), font5));
+                                tableComment.AddCell(new Phrase(drComment["strCommentText"].ToString(), font5));
+                            }
+                        }
+                        document.Add(tableComment);
                     }
-                    document.Add(tableComment);
+                    document.Close();
                 }
-                document.Close();
+                return "Report for " + fileName + " complete.";
             }
-            listBox1.Items.Add("Report for " + aReportPk.ToString() + " complete.");
+            else
+            {
+                return "No R number found.";
+            }
         }
 
         private string DateString(string s)
@@ -218,6 +229,7 @@ namespace HLAPtReportsExport
                 return " ";
             }
         }
+
     }
 
 
